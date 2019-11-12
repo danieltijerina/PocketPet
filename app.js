4b11dfaee8a1b39fb31f2ae64ce10ec24ee5583d
danieltijerina <dtg.danieltijerina@gmail.com>
2019-11-12 15:25:35 -0600

    Added endpoints.

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/users', function(req, res) {
 
+})
+
+app.get('/userByEmail/:email', function(req, res) {
+  _email = req.params.email
+
+})
+
+app.get('/pet/:ownerEmail/:petIndex', function(req, res) {
+  _email = req.params.ownerEmail
+  _index = req.params.petIndex
+
+})
+
+app.
 
 app.listen(port, function() {
   console.log('Server up and running on port', port)
