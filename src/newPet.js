5d7a47ae960f78771c83e9ba36fc80f038b7e958
LiteMasteralex <alexlara_97@hotmail.com>
2019-11-21 21:33:49 -0600

    Started the newPet Form

--- src/newPet.js
+++ src/newPet.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class newPet extends Component {
+class NewPet extends Component {
+	//Create Pet Forum
+	constructor(props) {
+		super(props);
+		this.pet = {
+			name: this.props.location.state.name,
+			color: '',
+			breed: '',
+			size: '',
+			weight: '',
+			species: '',
+			photo: '',
+			vaccines: []
+		};
 
+		this.vaccine = {
+			type: '',
+			application_date: new Date()
+		};
+	};
+    
     render() {
-        return (<div>
-            <h1>hello world</h1>
-        <p>hello {this.props.location.state.name}</p>
-            {console.log(this.props.location.state.name)}
-             </div>)
+        return (
+        	<div className="container">
+	        	<h1>Form</h1>
+			    <form onSubmit="{ this.savePet }">
+			      <div className="row">
+			        <div className="col-lg">
+			          <input type='text' value={this.pet.name} name='petName' placeHolder="Here goes the name"></input>
+			        </div>
+			        <div className="col-lg">
+			          <input type='text' value={this.pet.color} name='color' placeHolder="e.g. Brown, Golden, Red"></input>
+			        </div>
+			        <div className="col-lg">
+			          <input type='text' value={this.pet.name} name='petName' placeHolder="Here goes the name"></input>
+			        </div>
+			      </div>
+			      <div className="row">
+			        <div className="col-lg">
+			          <input type='text' value={this.pet.size} name='size' placeHolder="Small, Medium, Big"></input>
+			        </div>
+			        <div className="col-lg">
+			          <input type='number' value={this.pet.weight} name='weight' placeHolder="In Kgs"></input>
+			        </div>
+			        <div className="col-lg">
+			          <input type='text' value={this.pet.name} name='petName' placeHolder="Here goes the name"></input>
+			        </div>
+			      </div>
+			    </form>
+		    </div>
+	    )
     }
 }
 
-export default newPet;
+export default NewPet;
