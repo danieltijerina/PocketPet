c8d97a0160f4d8c4a822975edec8b0f0ecd95ea8
LiteMasteralex <alexlara_97@hotmail.com>
2019-11-25 09:32:39 -0600

    Added default essage for breeds
    Added preview image button

--- src/newPet.js
+++ src/newPet.js
@@ -34,6 +34,7 @@ class NewPet extends Component {
 			return response.json();
 		}).then(data => {
 			dogBreeds = data.message;
+			dogBreeds.unshift("Selecciona la raza.")
 			this.populateBreeds(this.state.species);
 		}).catch(err => {
 			//Alert user that there was an error retriving the list of breeds
@@ -44,6 +45,7 @@ class NewPet extends Component {
 			return response.json();
 		}).then(data => {
 			catBreeds = data.map(cat => cat.name);
+			catBreeds.unshift("Selecciona la raza.")
 			this.populateBreeds(this.state.species);
 		}).catch(err => {
 			//Alert user that there was an error retriving the list of breeds
@@ -51,6 +53,7 @@ class NewPet extends Component {
 		});
 		this.populateBreeds = (val) => {
 			this.setState({species: val}, function() {			
+				this.setState({breed: ''});
 				if(this.state.species === 'Perro') {
 					this.setState({breeds: dogBreeds});
 				} else if (this.state.species === 'Gato') {
@@ -99,7 +102,7 @@ class NewPet extends Component {
 			};
 			let vaccineList = this.state.vaccines;
 			vaccineList.push(vaccine);
-			this.setState({vaccines: vaccineList, type: '', application_date: new Date});
+			this.setState({vaccines: vaccineList, type: '', application_date: new Date()});
 		};
 
 		this.deleteRow = (index) => {
@@ -117,10 +120,11 @@ class NewPet extends Component {
 				</tr>
 			);
 		};
+		this.style = this.state.edit ? {} : {display: 'none'};
 	};
     
     render() {
-    	const style = this.state.edit ? {disply: 'none'} : {};
+    	const disButton = this.state.photo !== '' ? {} : {display: 'none'};	
 
         return (
         	<div>
@@ -166,6 +170,10 @@ class NewPet extends Component {
 				      		<label htmlFor="photo">Imagen</label>
 				      		<input className="form-control form-control-lg" type='text' name='photo' value={this.state.photo} placeholder="Url de la imagen" onChange={this.updateValue}></input>
 				      	</div>
+				      	<div className="col-md-4" style={disButton}>
+				      		<br/>
+				      		<a className='btn btn-primary btn-lg' href={this.state.photo} target="_blank">Vista Previa</a>
+				      	</div>
 				      </div>
 				      <div className="form-group">
 				      	<div className="col-sm-3">
@@ -176,7 +184,7 @@ class NewPet extends Component {
 				      </div>
 				    </form>
 			    </div>
-			    <div className="container" style={style}>
+			    <div className="container" style={this.style}>
 			    	<h2>Vacunas</h2>
 			    	<table className='table'>
 			    		<thead>
